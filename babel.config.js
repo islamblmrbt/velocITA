f7d54b297df9d3db21634817ebf7dd85fb8cccba
unknown <belmerabet@gmail.com>
2026-05-07 11:44:06 +0200

    babel fix

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // Each plugin should be a string or an array [name, options]
+      'react-native-reanimated/plugin',
     ],
   };
 };
